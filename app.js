3655ea9b642f17aeec7c336f9d98526bf74bd663
srikar-muthyala <muthyala.srikar20@gmail.com>
2020-01-20 21:35:28 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+
+
+app.get("/", function(req, res) {
+  var today = new Date();
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("en-US",options);
+
+  res.render("list", {kindOfDay: day});
+});
+
+app.listen(3000, function() {
+  console.log("server started successfully!");
+});
